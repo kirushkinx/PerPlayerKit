d6ceafd7a2435b7e8c2d9f00f87d470a5dd5b026
kirushkinx <138403498+kirushkinx@users.noreply.github.com>
2025-03-31 04:48:15 +0300

    changed for new gui

--- src/main/java/dev/noah/perplayerkit/listeners/KitMenuCloseListener.java
+++ src/main/java/dev/noah/perplayerkit/listeners/KitMenuCloseListener.java
@@ -38,10 +38,10 @@ public class KitMenuCloseListener implements Listener {
         if (inv.getSize() == 54) {
             if (inv.getLocation() == null) {
                 InventoryView view = e.getView();
-                if (view.getTitle().contains(ChatColor.BLUE + "Kit: ")) {
+                if (view.getTitle().contains(ChatColor.BLUE + "Kit ")) {
                     Player p = (Player) e.getPlayer();
                     UUID uuid = p.getUniqueId();
-                    int slot = Integer.parseInt(view.getTitle().replace(ChatColor.BLUE + "Kit: ", ""));
+                    int slot = Integer.parseInt(view.getTitle().replace(ChatColor.BLUE + "Kit ", ""));
                     ItemStack[] kit = new ItemStack[41];
                     ItemStack[] chestitems = e.getInventory().getContents();
 
@@ -55,7 +55,6 @@ public class KitMenuCloseListener implements Listener {
                     }
                     KitManager.get().savekit(uuid, slot, kit);
 
-
                 }
             }
         }
@@ -67,9 +66,9 @@ public class KitMenuCloseListener implements Listener {
         if (inv.getSize() == 54) {
             if (inv.getLocation() == null) {
                 InventoryView view = e.getView();
-                if (view.getTitle().contains(ChatColor.BLUE + "Public Kit: ")) {
+                if (view.getTitle().contains(ChatColor.BLUE + "Public Kit ")) {
                     Player player = (Player) e.getPlayer();
-                    String publickit = view.getTitle().replace(ChatColor.BLUE + "Public Kit: ", "");
+                    String publickit = view.getTitle().replace(ChatColor.BLUE + "Public Kit ", "");
                     ItemStack[] kit = new ItemStack[41];
                     ItemStack[] chestitems = e.getInventory().getContents();
 
@@ -92,21 +91,21 @@ public class KitMenuCloseListener implements Listener {
     @EventHandler
     public void onEnderchestEditorClose(InventoryCloseEvent e) {
         Inventory inv = e.getInventory();
-        if (inv.getSize() == 54) {
+        if (inv.getSize() == 36) {
             if (inv.getLocation() == null) {
                 InventoryView view = e.getView();
-                if (view.getTitle().contains(ChatColor.BLUE + "Enderchest: ")) {
+                if (view.getTitle().contains(ChatColor.BLUE + "Enderchest ")) {
                     Player p = (Player) e.getPlayer();
                     UUID uuid = p.getUniqueId();
-                    int slot = Integer.parseInt(view.getTitle().replace(ChatColor.BLUE + "Enderchest: ", ""));
+                    int slot = Integer.parseInt(view.getTitle().replace(ChatColor.BLUE + "Enderchest ", ""));
                     ItemStack[] kit = new ItemStack[27];
                     ItemStack[] chestitems = e.getInventory().getContents();
 
                     for (int i = 0; i < 27; i++) {
-                        if (chestitems[i + 9] == null) {
+                        if (chestitems[i] == null) {
                             kit[i] = null;
                         } else {
-                            kit[i] = chestitems[i + 9].clone();
+                            kit[i] = chestitems[i].clone();
                         }
 
                     }
